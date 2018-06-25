cce7c749d498ad3c68502967b5181146533502b4
Jesse Atay <jesse.atay@gmail.com>
2018-06-24 22:43:55 -0700

    initial files updates

---
+++ StudentEnrollment/StudentEnrollment/Data/SpyDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentEnrollment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Data
+{
+    public class SpyDBContext: DbContext
+    {
+		public SpyDBContext(DbContextOptions<SpyDBContext> options) : base(options)
+		{
+			
+		}
+
+		public DbSet<Course>Courses { get; set; }
+		public DbSet<Student>Students { get; set; }
+    }
+}
---
+++ StudentEnrollment/StudentEnrollment/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Models
+{
+    public class Course
+    {
+		public int ID { get; set; }
+    }
+}
