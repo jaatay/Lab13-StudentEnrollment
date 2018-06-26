d64b3b459b41b3cc63b537b026ddb51aa2fe80ea
Jesse Atay <jesse.atay@gmail.com>
2018-06-26 00:01:48 -0700

    Course CRUD updates

---
+++ StudentEnrollment/StudentEnrollment/Controllers/Course/CourseController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentEnrollment.Data;
+using StudentEnrollment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentEnrollment.Controllers.Course
+{
+	public class CourseController : Controller
+	{
+		private SpyDBContext _context;
+
+		public CourseController(SpyDBContext context)
+		{
+			_context = context;
+		}
+
+		//create
+		public async Task<IActionResult> Create()
+		{
+			ViewData["Courses"] = await _context.Courses.Select(x => x)
+				.ToListAsync();
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create([Bind("ID, Name, Level, Category")]StudentEnrollment.Models.Course course)
+		{
+			_context.Courses.Add(course);
+			await _context.SaveChangesAsync();
+			return RedirectToAction("Index", "Home");
+		}
+
+		//read
+
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id.HasValue)
+			{
+				
+				return View(await _context.Courses.Where(s => s.ID == id)
+					.SingleAsync());
+			}
+			return View();
+
+		}
+		public async Task<IActionResult> ViewAll()
+		{
+			var data = await _context.Courses.ToListAsync();
+
+			return View(data);
+
+		}
+
+		//update
+		[HttpGet]
+		public async Task<IActionResult> Update(int? id)
+		{
+			if (id.HasValue)
+			{
+				Models.Course course = await _context.Courses.FirstOrDefaultAsync(a => a.ID == id);
+				return View(course);
+			}
+			return RedirectToAction("Index", "Home");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Update([Bind("ID, Name, Level, Category")]Models.Course course)
+		{
+			_context.Courses.Update(course);
+
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction("Index", "Home");
+		}
+
+		//delete
+		public async Task<IActionResult> Delete(int id)
+		{
+			var course = await _context.Courses.FindAsync(id);
+
+
+			if (course == null)
+			{
+				return NotFound();
+			}
+
+			_context.Courses.Remove(course);
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction("Index", "Home");
+		}
+	}
+
+}
